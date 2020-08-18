77015537cf42cf3356708454adc6dfb2718e89f4
Olivia Mattiazzo <oli.pmatt@gmail.com>
2020-08-18 17:30:48 +0100

    Inserção rodapé com créditos e versionamento

--- js/views/ExportacaoView.js
+++ js/views/ExportacaoView.js
@@ -13,16 +13,23 @@ class ExportacaoView  {
                     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
                     <script src="https://kit.fontawesome.com/293847d6a2.js" crossorigin="anonymous"></script>
                 </head>
-                <body onload="exportacaoController.load()">
+                <body>
                     ${this._viewConjugacion.templateExportacao(this._listaConjugacionModel)}
                     
+                    <div id="footerView"></div>
+
+                    <script src="js/controllers/ExportacaoController.js"></script>
                     <script src="js/views/View.js"></script>
                     <script src="js/views/ConjugacionView.js"></script>
-                    <script src="js/controllers/ExportacaoController.js"></script>
                     <script src="js/views/ExportacaoView.js"></script>
+                    <script src="js/views/FooterView.js"></script>
                     <script>
+                        let footerView = new FooterView(document.getElementById('footerView'));
+                        footerView.update();
+
                         let exportacaoController = new ExportacaoController();
                     </script>
+                    <div id="footerView"></div>
                 </body>
             </html>
             `;
---
+++ js/views/FooterView.js
@@ -0,0 +1,7 @@
+class FooterView extends View {
+    template() {
+        return `
+            <footer><a href="https://oliviamattiazzo.github.io/AuxiliarPronombresEspanol/">Automatizador de Pronombres</a> por <a href="https://oliviamattiazzo.dev/" target="_blank">Olivia Mattiazzo</a> - versión 1.1.0</footer>
+        `;
+    }
+}
